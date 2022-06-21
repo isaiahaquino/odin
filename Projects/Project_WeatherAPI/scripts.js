f0005f4392f9953c3ae714683f1743a388f19e0c
Zay Macbook <isaiahaquino1997@gmail.com>
2022-06-21 14:40:35 -0700

    Project: Weather App; Fully functional

--- Projects/Project_WeatherAPI/scripts.js
+++ Projects/Project_WeatherAPI/scripts.js
@@ -9,6 +9,7 @@ const WeatherReport = (() => {
     let currentWeatherCondition = null;     // weather.main
 
     // Second box (list[0-7])
+    const hoursToShow = 8;
     let hourTemps = [];                     // list.main.temp
     let hourWeatherConditions = [];         // list.weather.main
     let hourTimes = [];                     // list.dt
@@ -31,6 +32,24 @@ const WeatherReport = (() => {
         console.log(`Day: ${dayDay} Temps: ${dayTemps}; Weathers: ${dayWeatherConditions}`);
     }
 
+    const getData = () => {
+        return { currentCity, currentTemp, currentWeatherCondition,
+                hourTemps, hourWeatherConditions, hourTimes,
+                dayTemps, dayDay, dayWeatherConditions, daysToShow, hoursToShow }
+    }
+
+    const reset = () => {
+        currentCity = null;         
+        currentTemp = null;               
+        currentWeatherCondition = null;     
+        hourTemps = [];                 
+        hourWeatherConditions = [];         
+        hourTimes = [];                   
+        dayTemps = [];                
+        dayWeatherConditions = [];    
+        dayDay = []; 
+    }
+
     const parseResponseCurr = (response) => {
         currentCity = response.name;
         currentTemp = Math.round(response.main.temp);
@@ -38,14 +57,14 @@ const WeatherReport = (() => {
     }
 
     const parseResponseDays = (response) => {
-        for (let i=0; i<8; i++) {
+        for (let i=0; i<hoursToShow; i++) {
             hourTemps[i] = Math.round(response.list[i].main.temp);
             hourWeatherConditions[i] = response.list[i].weather[0].main;
             hourTimes[i] = new Date(response.list[i].dt * 1000).getUTCHours();
             hourTimes[i] = `${hourTimes[i]}:00`;
         }
 
-        for (let i=0; i<5; i++) {
+        for (let i=0; i<daysToShow; i++) {
             let j = (i * 8) + 3;
             dayTemps[i] = Math.round(response.list[j].main.temp);
             dayWeatherConditions[i] = response.list[j].weather[0].main;
@@ -63,6 +82,7 @@ const WeatherReport = (() => {
             })
             .then(response => {
                 parseResponseCurr(response);
+                DisplayControl.displayReport();
             })
             .catch(error => {
                 console.log("Error: " + error);
@@ -75,7 +95,7 @@ const WeatherReport = (() => {
             })
             .then(response => {
                 parseResponseDays(response);
-                outputDataTest();
+                DisplayControl.displayReport();
             })
             .catch(error => {
                 console.log("Error: " + error);
@@ -87,7 +107,7 @@ const WeatherReport = (() => {
         currentCity = newCity;
     }
 
-    return {getWeather, changeCity, outputDataTest};
+    return {getWeather, changeCity, outputDataTest, getData};
 })();
 
 const DisplayControl = (() => {
@@ -101,7 +121,90 @@ const DisplayControl = (() => {
         WeatherReport.getWeather();
     }
 
-    return {search};
+    const resetReport = () => {
+        removeChildren(document.querySelector('.current'));
+        removeChildren(document.querySelector('.hourlyContainer'));
+        removeChildren(document.querySelector('.dailyContainer'));
+
+    }
+
+    const removeChildren = (parent) => {
+        while (parent.firstChild) {
+            parent.removeChild(parent.firstChild);
+        }
+    }
+
+    const displayReport = () => {
+        resetReport();
+        const report = WeatherReport.getData();
+
+        // Current
+        const parent = document.querySelector('.current')
+        const currentTemp = document.createElement('h1');
+        currentTemp.classList.add('currentTemp');
+        currentTemp.innerHTML = `${report.currentTemp}&#176`;
+        parent.appendChild(currentTemp);
+
+        const currentCity = document.createElement('h2');
+        currentCity.classList.add('currentCity');
+        currentCity.innerHTML = report.currentCity;
+        parent.appendChild(currentCity);
+
+        const imgContainer = document.createElement('div');
+        const img = document.createElement('img');
+        img.classList.add('currentImage');
+        img.src = `photos/${report.currentWeatherCondition}.png`;
+        imgContainer.appendChild(img);
+        parent.appendChild(imgContainer);
+
+        // Hourly
+        for (let i=0; i<report.hoursToShow; i++) {
+            const parent = document.createElement('div');
+            parent.classList.add('hourlyReport');
+
+            const temp = document.createElement('p');
+            temp.classList.add('hourlyTemp');
+            temp.innerHTML = `${report.hourTemps[i]}&#176`;
+            parent.appendChild(temp);
+
+            const weather = document.createElement('img');
+            weather.classList.add('hourlyWeather');
+            weather.src = `photos/${report.hourWeatherConditions[i]}.png`;
+            parent.appendChild(weather);
+
+            const time = document.createElement('p');
+            time.classList.add('hourlyTime');
+            time.innerHTML = report.hourTimes[i];
+            parent.appendChild(time);
+
+            document.querySelector('.hourlyContainer').appendChild(parent);
+        }
+
+        // Daily
+        for (let i=0; i<report.daysToShow; i++) {
+            const parent = document.createElement('div');
+            parent.classList.add('dailyReport');
+
+            const temp = document.createElement('p');
+            temp.classList.add('dailyTemp');
+            temp.innerHTML = `${report.dayTemps[i]}&#176`;
+            parent.appendChild(temp);
+
+            const weather = document.createElement('img');
+            weather.classList.add('dailyWeather');
+            weather.src = `photos/${report.dayWeatherConditions[i]}.png`;
+            parent.appendChild(weather);
+
+            const day = document.createElement('p');
+            day.classList.add('dailyDay');
+            day.innerHTML = report.dayDay[i];
+            parent.appendChild(day);
+
+            document.querySelector('.dailyContainer').appendChild(parent);
+        }
+    }
+
+    return {search, displayReport, resetReport};
 })();
 
 WeatherReport.getWeather();
