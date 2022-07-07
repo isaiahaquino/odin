ed440cc2c8f8c746dbfed3ebaacb67a0cd13a2bd
Isaiah Aquino <iaquino3181@sdsu.edu>
2022-07-07 11:15:42 -0700

    Project: Shopping Cart; setup router

--- Projects/project_shopping-cart/src/App.js
+++ Projects/project_shopping-cart/src/App.js
@@ -1,7 +1,9 @@
+import './styles/App.css';
+
 function App() {
   return (
     <div>
-     
+      
     </div>
   );
 }
---
+++ Projects/project_shopping-cart/src/components/Header.js
@@ -0,0 +1,12 @@
+function Header() {
+    return (
+        <header>
+            <h1>ShopName</h1>
+            <nav>
+                
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
