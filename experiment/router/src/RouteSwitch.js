6c10eef314fc3492c9e71eee075098c01ef5cf36
Isaiah Aquino <iaquino3181@sdsu.edu>
2022-07-07 08:43:17 -0700

    experiement with router

---
+++ experiment/router/src/RouteSwitch.js
@@ -0,0 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App";
+import Profile from "./Profile";
+import React from 'react';
+
+const RouteSwitch = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default RouteSwitch;
