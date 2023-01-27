25e28c34517fc548bdd8352511d9b307dce72722
isaiahaquino <isaiahaquino1997@gmail.com>
2023-01-27 11:00:13 -0800

    Express tutorial part 6

--- nodejs/express-locallibrary-tutorial/models/author.js
+++ nodejs/express-locallibrary-tutorial/models/author.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const { DateTime } = require("luxon");
 
 const Schema = mongoose.Schema;
 
@@ -28,6 +29,18 @@ AuthorSchema.virtual("url").get(function () {
   // We don't use an arrow function as we'll need the this object
   return `/catalog/author/${this._id}`;
 });
+
+AuthorSchema.virtual("date_of_birth_formatted").get(function () {
+  return this.date_of_birth ? DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.toISODate) : '';
+});
+
+AuthorSchema.virtual("date_of_death_formatted").get(function () {
+  return this.date_of_death ? DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_SHORT) : '';
+});
+
+AuthorSchema.virtual("lifespan").get(function () {
+  return `${this.date_of_birth ? DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED) : ''} - ${this.date_of_death ? DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED) : ''}`
+})
 
 // Export model
 module.exports = mongoose.model("Author", AuthorSchema);
